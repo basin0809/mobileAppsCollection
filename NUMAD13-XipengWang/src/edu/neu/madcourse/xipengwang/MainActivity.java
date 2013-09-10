97eb14282479decf8bcd3b4080825e1788ca0e47
XipengWang <wang.xip@husky.neu.edu>
2013-09-10 00:51:27 -0400

    add Warning.java and warning.xml

--- NUMAD13-XipengWang/src/edu/neu/madcourse/xipengwang/MainActivity.java
+++ NUMAD13-XipengWang/src/edu/neu/madcourse/xipengwang/MainActivity.java
@@ -6,6 +6,7 @@ import edu.neu.mobileClass.*;
 
 import android.R.integer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -23,16 +24,38 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		PhoneCheckAPI.doAuthorization(this);
-		aboutButton = (Button)findViewById(R.id.about_button);
-		sudokuButton = (Button)findViewById(R.id.sudoku_button);
-		errorButton = (Button)findViewById(R.id.error_button);
-		quitButton = (Button)findViewById(R.id.exit_button);
-		sudokuButton.setOnClickListener(new SudokuButtonListener());
-		aboutButton.setOnClickListener(new AboutButtonListener());
-		errorButton.setOnClickListener(new ErrorButtonListener());
-		quitButton.setOnClickListener(new ExitButtonListener());
+		
+		try {
+			PhoneCheckAPI.doAuthorization(this);
+			aboutButton = (Button)findViewById(R.id.about_button);
+			sudokuButton = (Button)findViewById(R.id.sudoku_button);
+			errorButton = (Button)findViewById(R.id.error_button);
+			quitButton = (Button)findViewById(R.id.exit_button);
+			sudokuButton.setOnClickListener(new SudokuButtonListener());
+			aboutButton.setOnClickListener(new AboutButtonListener());
+			errorButton.setOnClickListener(new ErrorButtonListener());
+			quitButton.setOnClickListener(new ExitButtonListener());
+		} catch (SecurityException e) {
+			// TODO: handle exception
+			Intent intent =new Intent();
+			intent.setClass(MainActivity.this, Warning.class);
+			startActivity(intent);
+			Handler handler = new Handler();
+			Runnable runnable = new Runnable() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					finish();
+				}
+			};
+			handler.postDelayed(runnable, 3000);
+		}
+		
+		
+		
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
